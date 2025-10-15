d353063eb214ce2c0bc3302d575c03e65d4ed721
LeonardoSantos47 <leoosantoos47@gmail.com>
2025-10-15 14:28:14 -0300

    Update socket.js

--- client/js/socket.js
+++ client/js/socket.js
@@ -1,6 +1,6 @@
 // Socket.IO client connection and event handling
 
-const DEFAULT_TUNNEL_URL = 'https://assistance-den-behalf-height.trycloudflare.com';
+const DEFAULT_TUNNEL_URL = 'https://tuning-midnight-highways-vids.trycloudflare.com';
 
 class SocketManager {
     constructor() {
